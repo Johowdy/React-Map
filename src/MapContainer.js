25d6da2c3a5e7d6298d4ea02b3010be2319fd815
Jodie-Houghton <jodiehoughto@gmail.com>
2019-01-05 13:01:26 -0700

    create-react-app initialization and blank MapContainer component

---
+++ src/MapContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MapContainer extends Component {
+  render() {
+    return (
+      <div id="map">
+        Here is where the map will go
+      </div>
+    )
+  }
+}
+
+export default MapContainer;

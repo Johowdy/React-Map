bd2d0852e67c1869a940bb5986c53f3984b16984
Jodie-Houghton <jodiehoughto@gmail.com>
2019-01-06 17:05:13 -0700

    added initial Google Maps API calls and LocationsList

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MapContainer from './MapContainer'
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,10 +1,71 @@
 import React, { Component } from 'react';
+import LocationsList from './LocationsList';
 
 class MapContainer extends Component {
+  state = {
+    selectedLocation: null,
+    locations: []
+  }
+
+  initGoogleMaps() {
+    if (!this.mapsPromise) {
+      this.mapsPromise = new Promise((resolve) => {
+        window.resolveGoogleMaps = () => {
+          resolve(window.google);
+
+          delete window.resolveGoogleMaps;
+        };
+
+        const script = document.createElement("script");
+        const apiKey = 'AIzaSyBihJVaCxpU8TqNPiGRomIxS8pH6QEtLa0';
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=resolveGoogleMaps&libraries=places`;
+        script.async = true;
+        document.body.appendChild(script);
+      })
+    }
+    return this.mapsPromise;
+  };
+
+  componentWillMount() {
+    this.initGoogleMaps();
+  }
+
+  componentDidMount() {
+    this.initGoogleMaps().then((google) => {
+      console.log('google', google);
+      this.map = new google.maps.Map(document.getElementById('map'), {zoom: 11});
+      this.geocoder = new google.maps.Geocoder();
+      this.geocoder.geocode({address: 'El Paso, TX'}, (results, status) => {
+        if (status === 'OK') {
+          this.map.setCenter(results[0].geometry.location);
+          this.populateVets();
+        } else {
+          alert('could not geocode El Paso: '+status);
+        }
+      })
+    })
+  }
+
+  populateVets() {
+    this.placesService = new window.google.maps.places.PlacesService(this.map);
+    this.placesService.textSearch({query: 'vet in El Paso, TX'}, (results, status) => {
+      this.setState({locations:results});
+    })
+  }
+
+  selectLocation(location) {
+    this.setState({selectedLocation: location})
+  }
+
   render() {
+    const { locations, selectedLocation } = this.state;
     return (
-      <div id="map">
-        Here is where the map will go
+      <div id="map-container">
+        <div id="header">
+          Vets in El Paso, Tx
+        </div>
+        <LocationsList locations={locations} selectedLocation={selectedLocation} selectLocation={location => this.selectLocation(location)}/>
+        <div id="map">...Loading...</div>
       </div>
     )
   }

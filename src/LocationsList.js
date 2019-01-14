272f20e23f03cb16b7bc7736bf222d9041c17a38
Jodie-Houghton <jodiehoughto@gmail.com>
2019-01-13 18:27:54 -0700

    added some responsive css

--- src/LocationsList.js
+++ src/LocationsList.js
@@ -3,25 +3,34 @@ import React, { Component } from 'react';
 class LocationsList extends Component {
 
   render() {
-    const { selectedLocation, selectLocation, locations, updateFilter, filter } = this.props;
+    const { selectedLocation, selectLocation, locations, updateFilter, filter, hidden, locationsLoading } = this.props;
 
     return (
-      <div id="locations-list">
+      <section id="locations-list" className={hidden ? 'hidden' : null}>
         <div className="filter-locations">
-          <input type="text" placeholder="Filter" value={filter} onChange={event => updateFilter(event.target.value)}/>
+          <input
+            type="text"
+            placeholder="Filter" value={filter}
+            onChange={event => updateFilter(event.target.value)}/>
         </div>
         <div className="locations-results">
           <ul>
-            {locations.map(location => {
+            {locations && locations.map(location => {
               return (
-                <li key={location.id} location={location} onClick={() => selectLocation(location)} className={selectedLocation && selectedLocation.id === location.id ? 'selected' : null}>
+                <li
+                  key={location.id}
+                  onClick={() => selectLocation(location)}
+                  className={selectedLocation && selectedLocation.id === location.id ? 'selected' : null}>
                 {location.name}
                 </li>
               )
             })}
+            {locationsLoading &&
+                <li>...loading...</li>
+            }
           </ul>
         </div>
-      </div>
+      </section>
     )
   }
 
